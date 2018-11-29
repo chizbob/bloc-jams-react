f906c66924f772f4f9a3757b55f080f3cc1fc0de
Soo Kim <chizbob@gmail.com>
2018-11-28 18:35:32 -0800

    playback assignment completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,16 +3,47 @@ import albumData from './../data/albums';
 
 class Album extends Component {
   constructor(props) {
-     super(props);
+       super(props);
 
-     const album = albumData.find( album => {
-        return album.slug === this.props.match.params.slug
-      });
+       const album = albumData.find( album => {
+          return album.slug === this.props.match.params.slug;
+        })
 
-      this.state = {
-        album: album
-      };
+       this.state = {
+          album: album,
+          currentSong: album.songs[0],
+          isPlaying: false
+       }
+
+       this.audioElement = document.createElement('audio');
+       this.audioElement.src = album.songs[0].audioSrc;
   }
+
+   play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true });
+   }
+
+   pause() {
+     this.audioElement.pause();
+     this.setState({ isPlaying: false });
+   }
+
+   setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+   }
+
+   handleSongClick(song) {
+       const isSameSong = this.state.currentSong === song;
+       if (this.state.isPlaying && isSameSong) {
+          this.pause();
+       } else {
+         if (!isSameSong){
+          this.setSong(song)}
+          this.play();
+       }
+   }
 
   render() {
     return (
@@ -32,10 +63,18 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+            { this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+                <td>{index+1}</td>
+                <td>{song.title}</td>
+                <td>{song.duration} sec</td>
+              </tr>
+             )
+            }
           </tbody>
         </table>
       </section>
-    );
+    )
   }
 }
 
